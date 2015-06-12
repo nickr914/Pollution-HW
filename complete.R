83974a8166c23c631045f8512aef894c20862e92
Nick R <nickr914@yahoo.com>
2015-06-12 11:41:18 -0400

    Removed extra data

--- complete.R
+++ complete.R
@@ -1,22 +1,20 @@
 #complete.R Nick R
 
-complete <- function(directory, id = 1:332) {
+complete <- function(directory ="specdata", id = 1:332) {
+  nobs<-vector()
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the CSV files
   files <- list.files(directory, pattern="*.csv", full.names = TRUE)
   for (i in id) {
-    #specdata <- rbind(specdata, read.csv(files[i]))
     nobs[i]<-sum(complete.cases(read.csv(files[i])))
     
   }
   ## 'id' is an integer vector indicating the monitor ID numbers
   ## to be used
-  #nobs[i]<-nrow(na.omit(specdata))
-#  nobs<-nrow(na.omit(specdata))
   nobtable <- data.frame(cbind(id,nobs=nobs[id]))
   # Add the value to the end of the vector
-# Take a look at the final vector you end up with
-#  nobtable <- data.frame(cbind(id,nobs=nobs[i]))
+  # Take a look at the final vector you end up with
+  #  nobtable <- data.frame(cbind(id,nobs=nobs[i]))
   return(nobtable)
   ## Return a data frame of the form:
   ## id nobs
